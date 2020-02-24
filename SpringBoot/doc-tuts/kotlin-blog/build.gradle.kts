3d4ae27981b53d1924ddd6f163d694249aee8f6d
Kamil Cinner <kamil.cinner@gmail.com>
2020-02-24 17:42:54 +0100

    Finished doc-tut

--- SpringBoot/doc-tuts/kotlin-blog/build.gradle.kts
+++ SpringBoot/doc-tuts/kotlin-blog/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("plugin.spring") version "1.3.61"
     kotlin("plugin.jpa") version "1.3.61"
     kotlin("plugin.allopen") version "1.3.61"
+    kotlin("kapt") version "1.3.61"
 }
 
 allOpen {
@@ -49,6 +50,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("com.ninja-squad:springmockk:1.1.3")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<Test> {
