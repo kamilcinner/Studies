33d90064e2d8c091a20a13200870e62c67f76242
Kamil Cinner <kamil.cinner@gmail.com>
2020-03-04 16:35:15 +0100

    Added some code and removed trash

--- Python/scheduler-parser-demo/main.py
+++ Python/scheduler-parser-demo/main.py
@@ -1,39 +1,79 @@
+import datetime
+
 from bs4 import BeautifulSoup
-import requests
 import os
 from urllib import request
 
 class Subject:
-    def __init__(self, name=None, lecturer=None, class_=None, time=None):
+    def __init__(self, name=None, lecturer=None, class_=None, time_start=None, time_end=None):
         self.name = name
         self.lecturer = lecturer
         self.class_ = class_
-        self.time = time
+        self.time_start = time_start
+        self.time_end = time_end
 
 def main():
-    url = 'http://elektron.pol.lublin.pl/ats4/plan.php?type=0&id=6420&winW=943&winH=852&loadBG=000000'
+    url = 'http://we1.pollub.pl/ats4/plan.php?type=0&id=6200&winW=943&winH=852&loadBG=000000'
     req = request.urlopen(url).read().decode('UTF-8')
     soup = BeautifulSoup(req, 'html.parser')
-    #result = soup.find_all(id='course_6')
     result = soup.find_all('div', class_='coursediv')
     r2 = BeautifulSoup(str(result), 'html.parser')
-    #print(r2.contents)
-    #print(type(r2.text))
-    #print(type(r2))
-    #print(r2.text)
+
     tab = []
+    subjects = []
     for r in r2.contents:
         soup = BeautifulSoup(str(r), 'html.parser')
-        #print(soup.text)
         if soup.text != ', ':
-            tab.append(soup.text)
-            subject = Subject()
-            
-        #print(type(soup.text))
+            tab.append(soup.contents)
+            for i in soup.contents:
+                strings = BeautifulSoup(str(i), 'html.parser').strings
+                my_strings = []
+                for s in strings:
+                    if s != '\n' and s != '[' and s != ']':
+                        my_strings.append(str(s))
+                if len(my_strings) > 3:
+                    subject = Subject()
+                    subject.name = str(my_strings[0])
+                    if subject.name.find(', lekt') != -1:
+                        subject.lecturer = 'Not defined'
+                        subject.class_ = 'Not defined'
+                    else:
+                        subject.lecturer = str(my_strings[1])
+                        subject.class_ = str(my_strings[2])
+                        
+                    time = my_strings[len(my_strings)-1]
+                    subject.time_start = str(time[:5])
+                    subject.time_end = str(time[8:13])
+                    
+                    print(subject.name, type(subject.name))
+                    print(subject.lecturer, type(subject.lecturer))
+                    print(subject.class_, type(subject.class_))
+                    print(datetime.datetime.strptime(subject.time_start, '%H:%M').time(), type(subject.time_start))
+                    print(subject.time_end, type(subject.time_end))
+                    print('\n')
 
-    for s in tab:
-        print(s)
-    print(tab)
+                    subjects.append(subject)
+                elif len(my_strings) > 2:
+                    subject = Subject()
+                    subject.name = str(my_strings[0])
+
+                    subject.lecturer = 'Not defined'
+                    subject.class_ = str(my_strings[1])
+
+                    time = str(my_strings[2])
+                    subject.time_start = str(time[:5])
+                    subject.time_end = str(time[8:13])
+
+                    print(subject.name, type(subject.name))
+                    print(subject.lecturer, type(subject.lecturer))
+                    print(subject.class_, type(subject.class_))
+                    print(datetime.datetime.strptime(subject.time_start, '%H:%M').time(), type(subject.time_start))
+                    print(subject.time_end, type(subject.time_end))
+                    print('\n')
+
+                    subjects.append(subject)
+
+
 
     data = ''
     file_path = os.path.join('.', 'data.txt')
