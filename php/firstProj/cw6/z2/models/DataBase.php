e9e8e6ef52dcf1b39f42930ff5d05bd8731f3eb0
Kamil Cinner <kamil.cinner@gmail.com>
2020-11-14 00:30:11 +0100

    Added some code

---
+++ php/firstProj/cw6/z2/models/DataBase.php
@@ -0,0 +1,91 @@
+<?php
+
+class DataBase {
+    private $mysqli;
+
+    public function __construct(string $serwer, string $user, string $pass, string $baza) {
+        $this->mysqli = new mysqli($serwer, $user, $pass, $baza);
+
+        if ($this->mysqli->connect_errno) {
+            printf("Nie udało sie połączenie z serwerem: %s\n",
+                $this->mysqli->connect_error);
+            exit();
+        }
+
+        $this->mysqli->set_charset("utf8");
+    }
+
+    function __destruct() {
+        $this->mysqli->close();
+    }
+
+    public function select(string $lang = '') {
+        $query = "select Surname, LanguageOrder from clients";
+        $columns = ['Surname', 'LanguageOrder'];
+
+        if ($lang != '') {
+            $query .= " where LanguageOrder regexp '$lang'";
+        }
+
+        $body = '';
+
+        if ($result = $this->mysqli->query($query)) {
+            $body .= '<table><tbody>';
+
+            while ($row = $result->fetch_object()) {
+                $body .= '<tr>';
+                for ($i = 0; $i < count($columns); $i++) {
+                    $col = $columns[$i];
+                    $body .= '<td>' . $row->$col . '</td>';
+                }
+                $body.='</tr>';
+            }
+            $body.='</table></tbody>';
+
+            $result->close();
+        }
+        return $body;
+    }
+
+    public function insert(string $surname, int $age, string $country, string $email, array $languages, string $payment) {
+        $languageSet = '';
+
+        foreach ($languages as $index => $lang) {
+            if ($index === 0) {
+                $languageSet .= $lang;
+            } else {
+                $languageSet .= ",$lang";
+            }
+        }
+        $query = "insert into clients values (null, '$surname', '$age', '$country', '$email', '$languageSet', '$payment')";
+
+        if ($conn = $this->mysqli->query($query)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function delete() {
+        $query = 'delete from clients';
+        if ($conn = $this->mysqli->query($query)) {
+            $conn->close();
+            return true;
+        }
+        return false;
+    }
+
+    public function getOrdersCountByAge(string $ageCondition = '') {
+        $count = false;
+
+        $query = "select count(Age) from clients";
+        if ($ageCondition != '') {
+            $query .= " where age $ageCondition";
+        }
+
+        if ($conn = $this->mysqli->query($query)) {
+            $count = $conn->fetch_row();
+            $conn->close();
+        }
+        return $count[0];
+    }
+}
---
+++ php/firstProj/cw6/z2/utils.php
@@ -0,0 +1,101 @@
+<?php
+include_once 'models/DataBase.php';
+
+$db = new DataBase("localhost:3306", "root", "", "clients");
+
+function add() {
+    echo '<h2>Validation info</h2>';
+    validateData();
+}
+
+function show() {
+    global $db;
+    echo $db->select();
+}
+
+function showOrder(string $lang) {
+    global $db;
+    echo $db->select($lang);
+}
+
+function showStats() {
+    global $db;
+    $ordersCount = $db->getOrdersCountByAge();
+    $ordersLess18 = $db->getOrdersCountByAge('< 18');
+    $ordersMore50 = $db->getOrdersCountByAge('> 50');
+
+    echo "Liczba wszystkich zamówień: $ordersCount<br>";
+    echo "Zamówienie od osób poniżej 18 lat: $ordersLess18<br>";
+    echo "Zamówienie od osób powyżej 50 lat: $ordersMore50<br>";
+}
+
+function validateData() {
+    global $db;
+
+    $regex = [
+        'lastname' => '/^[A-Z]{1}[a-ząęłńśćźżó-]{1,25}$/',
+        'country' => '(Polska|Niemcy|Wielka Brytania|Czechy)',
+        'tech' => '(Java|PHP|CPP)',
+        'payment' => '(Visa|Master Card|Przelew)'
+    ];
+
+    $args = [
+        'lastname' => [
+            'filter' => FILTER_VALIDATE_REGEXP,
+            'options' => [
+                'regexp' => $regex['lastname']
+            ]
+        ],
+        'age' => [
+            'filter' => FILTER_VALIDATE_INT
+        ],
+        'country' => [
+            'filter' => FILTER_VALIDATE_REGEXP,
+            'options' => [
+                'regexp' => $regex['country']
+            ]
+        ],
+        'email' => [
+            'filter' => FILTER_VALIDATE_EMAIL
+        ],
+        'tech' => [
+            'filter' => FILTER_VALIDATE_REGEXP,
+            'flags' => FILTER_FORCE_ARRAY,
+            'options' => [
+                'regexp' => $regex['tech']
+            ],
+        ],
+        'payment' => [
+            'filter' => FILTER_VALIDATE_REGEXP,
+            'options' => [
+                'regexp' => $regex['payment']
+            ]
+        ]
+    ];
+
+    $validatedData = filter_input_array(INPUT_POST, $args);
+
+    var_dump($validatedData);
+
+    $errors = '';
+    foreach ($validatedData as $key => $value) {
+        if (!$value) {
+            $errors .= $key . ', ';
+        }
+    }
+
+    if ($errors === '') {
+        echo '<h3 style="color: green;">Success</h3>';
+        $db->insert(
+            $validatedData['lastname'],
+            $validatedData['age'],
+            $validatedData['country'],
+            $validatedData['email'],
+            $validatedData['tech'],
+            $validatedData['payment'],
+        );
+    } else {
+        echo '<h3 style="color: red;">Fail</h3>';
+        echo '<br>Bad data provided: ' . $errors;
+    }
+}
