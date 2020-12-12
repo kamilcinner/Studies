a93f09d8c7461f35782040b2cc726e2e1e4c4c89
Kamil Cinner <kamil.cinner@gmail.com>
2020-12-12 16:06:39 +0100

    Added some code

---
+++ Python/bsi/class7-8-9-ex4/calculator.py
@@ -0,0 +1,12 @@
+def main():
+    nums = [
+    31007.70, 31356.05, 31222.33, 31277.74, 31511.89
+    ]
+    _sum = .0
+    for num in nums:
+        _sum += num
+    print(_sum/len(nums))
+
+
+if __name__ == "__main__":
+    main()
