fd2b7966308bad5f63f089138257792e15a08c53
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-08 18:18:06 +0100

    Added some code

--- oracle-db/lab9/9.3.sql
+++ oracle-db/lab9/9.3.sql
@@ -33,3 +33,98 @@ and customerkey not in (
     where to_char(oh.orderdate, 'mm-yyyy') = '01-2020'
 )
 order by 2;
+
+--3
+select distinct productname Product
+from product
+where productkey in (
+    select distinct od.productkey
+    from orderheader oh
+    inner join orderdetail od on od.orderkey = oh.orderkey
+    inner join country c on c.countrykey = oh.countrykey
+    inner join salesterritory st on st.salesterritorykey = c.salesterritorykey
+    where st.salesterritoryname = 'North America'
+)
+and productkey in (
+    select distinct od.productkey
+    from orderheader oh
+    inner join orderdetail od on od.orderkey = oh.orderkey
+    inner join country c on c.countrykey = oh.countrykey
+    inner join salesterritory st on st.salesterritorykey = c.salesterritorykey
+    where st.salesterritoryname = 'Australia'
+);
+
+--4 this solution gives 1807 records but is kinda sketchy
+select distinct
+c.customerkey as "Customer ID",
+c.lastname || ' ' || c.firstname as "Customer Name",
+co.countryname Country,
+coc.countryname ClientHomeCountry
+from orderheader oh
+inner join customer c on c.customerkey = oh.customerkey
+inner join city ci on ci.citykey = c.citykey
+inner join country coc on coc.countrykey = ci.countrykey
+inner join country co on co.countrykey = oh.countrykey
+where to_char(oh.orderdate, 'mm-yyyy') = '06-2020'
+and ci.countrykey != oh.countrykey
+order by 2, 3;
+
+--5
+select
+tab.year "Year",
+max(tab.order_number) as "#Max Monthy Orders"
+from
+(
+    select
+    extract(year from orderdate) year,
+    extract(month from orderdate) month,
+    count(*) order_number
+    from orderheader oh
+    group by
+    extract(year from orderdate),
+    extract(month from orderdate)
+) tab
+group by tab.year;
+
+--6
+select
+extract(year from order_date) Year,
+round(avg(order_value), 2) as "Avg Order Value"
+from (
+    select
+    oh.orderkey,
+    oh.orderdate order_date,
+    sum(od.transactionprice * od.quantity) order_value
+    from orderheader oh
+    inner join orderdetail od on od.orderkey = oh.orderkey
+    group by
+    oh.orderkey,
+    oh.orderdate
+)
+group by extract(year from order_date);
+
+--7
+select
+extract(year from oh.orderdate) Year,
+to_char(oh.orderdate, 'Month') Month,
+c.countryname Country,
+round(avg(t.order_value), 2) as "Avg Order Value"
+from orderheader oh
+inner join country c on c.countrykey = oh.countrykey
+inner join (
+    select
+    od.orderkey order_key,
+    sum(od.transactionprice * od.quantity) order_value
+    from orderdetail od
+    group by od.orderkey
+) t on t.order_key = oh.orderkey
+group by
+extract(year from oh.orderdate),
+to_char(oh.orderdate, 'Month'),
+c.countryname
+order by
+1,
+to_date(to_char(oh.orderdate, 'Month'), 'Month'),
+4 desc;
+
+--8
