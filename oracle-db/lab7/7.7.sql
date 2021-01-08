3ea2ae914ec774291d3c67c3c583792ef2eecc0c
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-08 16:20:33 +0100

    Fixed 7.7.1

--- oracle-db/lab7/7.7.sql
+++ oracle-db/lab7/7.7.sql
@@ -1,26 +1,18 @@
 --7.7.1
 select distinct
-case
-    when to_char(oh.orderdate, 'mm') <= 3 then 'Q1'
-    when to_char(oh.orderdate, 'mm') in ('07', '08', '09') then 'Q3'
-end as "Quarter",
+'Q' || to_char(oh.orderdate, 'q') Quarter,
 c.customerkey,
 c.lastname,
 c.firstname
 from customer c
-inner join orderheader oh
-on c.customerkey = oh.customerkey
-inner join orderchannel ch
-on oh.channelkey = ch.channelkey
-inner join orderdetail od
-on od.orderkey = oh.orderkey
-inner join product p
-on p.productkey = od.productkey
-inner join productsubcategory psc
-on psc.productsubcategorykey = p.productsubcategorykey
-inner join productcategory pc
-on pc.productCategoryKey = psc.productCategoryKey
+inner join orderheader oh on c.customerkey = oh.customerkey
+inner join orderchannel ch on oh.channelkey = ch.channelkey
+inner join orderdetail od on od.orderkey = oh.orderkey
+inner join product p on p.productkey = od.productkey
+inner join productsubcategory psc on psc.productsubcategorykey = p.productsubcategorykey
+inner join productcategory pc on pc.productCategoryKey = psc.productCategoryKey
 where pc.productCategoryName = 'Bikes'
-and ch.channelname = 'Mobile Application'
-and to_char(oh.orderdate, 'YYYY') = '2019'
+and ch.channelname = 'Mobile application'
+and to_char(oh.orderdate, 'yyyy') = '2019'
+and to_char(oh.orderdate, 'q') in ('1', '3')
 order by 1, 2;
