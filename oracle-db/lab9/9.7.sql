0dafad972f253127e3ec9a7c72ca1ab4dac98dfd
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-10 00:18:55 +0100

    Added some code

--- oracle-db/lab9/9.7.sql
+++ oracle-db/lab9/9.7.sql
@@ -17,8 +17,10 @@ select * from CustomerBackup;
 commit;
 
 --2
+-- copy (610 records)
 create table ProductBackup as select * from Product;
 
+-- delete
 delete from ProductBackup pb
 where pb.productkey not in (
     select distinct od.productkey
@@ -30,7 +32,7 @@ where pb.productkey not in (
     having sum(od.quantity) > 10
 );
 
--- check
+-- check (477 records)
 select * from Product
 minus
 select * from ProductBackup;
@@ -39,3 +41,61 @@ select * from ProductBackup;
 commit;
 
 --3
+-- delete
+delete from ProductBackup pb
+where pb.productkey in (
+    select distinct od.productkey
+    from orderdetail od
+    inner join orderheader oh on od.orderkey = oh.orderkey
+    where extract(year from oh.orderdate) = '2020'
+    group by
+    od.productkey
+    having sum(od.quantity) < 10
+);
+
+-- check
+select * from Product
+minus
+select * from ProductBackup;
+
+-- apply changes
+commit;
+
+--4
+-- copy
+create table CountryBackup as select * from Country;
+
+-- delete
+delete from CountryBackup cb
+where cb.countrykey in (
+    select min() from (
+        select
+        oh.countrykey,
+        count(*) orders_num
+        from orderheader oh
+        group by oh.countrykey
+    )
+)
+
+--5
+-- copy
+create table DeliveryMethodBackup as select * from DeliveryMethod;
+
+-- delete
+delete from DeliveryMethodBackup dmb
+where dmb.deliverymethodkey in (
+    select oh.deliverymethodkey
+    from orderheader oh
+    group by oh.deliverymethodkey
+    having count(distinct oh.customerkey) < 0.05 * (
+        select count(distinct toh.customerkey) from orderheader toh
+    )
+)
+
+-- check
+select * from DeliveryMethod
+minus
+select * from DeliveryMethodBackup;
+
+-- apply changes
+commit;
