d7018d6d79c327fd0385e621c1fd1f4adb18ad9c
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-09 21:58:53 +0100

    Added some code

--- oracle-db/lab9/9.6.sql
+++ oracle-db/lab9/9.6.sql
@@ -1,4 +1,5 @@
 --1
+-- update
 update orderheader set deliverycost = 0
 where orderkey in (
     select
@@ -24,6 +25,7 @@ group by oh.orderkey, oh.deliverycost;
 commit;
 
 --2
+-- update
 update orderheader set deliverydate = deliverydate + 1
 where orderkey in (
     select
@@ -58,6 +60,7 @@ and to_char(oh.orderdate, 'mm-yyyy') = '06-2020'
 and od.discountpctg < 10,
 order by 1;
 
+-- update
 update orderdetail set
 discountpctg = 15,
 discountamount = (0.15 * (transactionprice + discountamount)),
@@ -94,3 +97,45 @@ order by 1;
 commit;
 
 --4
+-- check
+select
+orderkey,
+productkey,
+transactionprice,
+quantity
+from orderdetail
+where orderkey in (
+    select od.orderkey
+    from orderdetail od
+    inner join orderheader oh on oh.orderkey = od.orderkey
+    where extract(year from oh.orderdate) = '2020'
+    group by od.orderkey
+    having sum(od.transactionprice * od.quantity) > 7300
+    and count(*) > 1
+)
+order by 1, 3 desc, 4 desc;
+
+-- update
+update orderdetail set quantity = quantity + 1
+where (orderkey, productkey) in (
+    select
+    od.orderkey,
+    od.productkey
+    from orderdetail od
+    where od.orderkey in (
+        select od.orderkey
+        from orderdetail od
+        inner join orderheader oh on oh.orderkey = od.orderkey
+        where extract(year from oh.orderdate) = '2020'
+        group by od.orderkey
+        having sum(od.transactionprice * od.quantity) > 7300
+        and count(*) > 1
+    )
+    and od.transactionprice = (
+        select min(tod.transactionprice)
+        from orderdetail tod
+        where tod.orderkey = od.orderkey
+    )
+);
+
+--5
