f6be1b821446d6bc8e85efb15a301de29cae87f0
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-09 23:35:40 +0100

    Added some code

--- oracle-db/lab9/9.7.sql
+++ oracle-db/lab9/9.7.sql
@@ -4,8 +4,7 @@ create table CustomerBackup as select * from customer;
 
 -- delete
 delete from CustomerBackup
-where customerkey not in
-(
+where customerkey not in (
     select distinct customerkey from orderheader
 );
 
@@ -18,3 +17,25 @@ select * from CustomerBackup;
 commit;
 
 --2
+create table ProductBackup as select * from Product;
+
+delete from ProductBackup pb
+where pb.productkey not in (
+    select distinct od.productkey
+    from orderdetail od
+    inner join orderheader oh on od.orderkey = oh.orderkey
+    group by
+    od.productkey,
+    extract(year from oh.orderdate)
+    having sum(od.quantity) > 10
+);
+
+-- check
+select * from Product
+minus
+select * from ProductBackup;
+
+-- apply changes
+commit;
+
+--3
