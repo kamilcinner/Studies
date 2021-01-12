514b20fc3042322c96d7f8d2038c85f53160b18a
Kamil Cinner <kamil.cinner@gmail.com>
2021-01-12 20:08:46 +0100

    Checked 7.4 and added fixes

--- oracle-db/lab7/7.4.sql
+++ oracle-db/lab7/7.4.sql
@@ -1,23 +1,19 @@
 --7.4.1
 select distinct
-c.countryname as "Country",
+c.countryname Country,
 oc.channelName as "Order channel"
 from orderHeader oh
-inner join orderChannel oc
-on oh.channelKey = oc.channelKey
-right join country c
-on c.countryKey = oh.countryKey
-order by 1 nulls first;
+inner join orderChannel oc on oh.channelKey = oc.channelKey
+right join country c on c.countryKey = oh.countryKey
+order by 1;
 
 --7.4.2
 select distinct
 ch.channelname,
 pm.paymentmethodname
 from orderheader oh
-inner join paymentmethod pm
-on oh.paymentmethodkey = pm.paymentmethodkey
-right join orderchannel ch
-on oh.channelkey = ch.channelkey;
+inner join paymentmethod pm on oh.paymentmethodkey = pm.paymentmethodkey
+right join orderchannel ch on oh.channelkey = ch.channelkey;
 
 --7.4.3
 select distinct
@@ -26,9 +22,6 @@ c.lastname,
 c.firstname,
 ch.channelname
 from orderchannel ch
-inner join orderheader oh
-on ch.channelkey = oh.channelkey
-right join customer c
-on c.customerkey = oh.customerkey
+inner join orderheader oh on ch.channelkey = oh.channelkey
+right join customer c on c.customerkey = oh.customerkey
 where c.lastname = 'Alan';
---to moze tez wyswietlic nulle, pytanie czy tak ma byc...
